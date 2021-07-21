7c6dafa66b7a132397537dc7150beeba329ce080
Naruto123489 <77681679+Naruto123489@users.noreply.github.com>
2021-07-21 17:45:53 -0400

    Add files via upload

---
+++ reminders.py
@@ -0,0 +1,6 @@
+print("-------------------------------------------------FIRST OF AUGEST WEEK MONDAY TO FRIDAY TASKES-----------------------------------------------")
+print("MONDAY: camping whole day")
+print("TUESDAY: Making sure I have all school supplices")
+print("WEDNESDAY: movies and baseball")
+print("THURSDAY: First day of school")
+print("FRIDAY: second shot for covid")
